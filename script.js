9befa18645cdfe61fffa8db3bed3aa005d25f2d5
vidyatiwrai <127404171+vidyatiwrai@users.noreply.github.com>
2023-05-03 19:53:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 /* Edit this file */
 const player = document.querySelector('.player');
+
 const video = player.querySelector('.viewer');
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+function togglePlay() {
+if (video.paused) {
+video.play();
+} else {
+video.pause();
+}
+}
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
